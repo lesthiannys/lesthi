e465af940bf4d366692f5360c1dfb0668d678359
lesthiannys <161051154+lesthiannys@users.noreply.github.com>
2024-11-06 11:37:40 -0300

    Add files via upload

---
+++ JS preguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    'programação',
+    'O que e Javascript',
+    'O JavaScript e uma linguagem de programação'
+)
+criaCartção(
+    'historia',
+    'Qual foi o ano da 2 guerra mundial',
+    '1939 a 1945',
+)
